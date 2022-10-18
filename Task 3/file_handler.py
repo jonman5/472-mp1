c344809eb503bc2c1855fed727b8735cfbd6c38b
Jonathan Perlman <31137025+jonman5@users.noreply.github.com>
2022-10-18 01:37:49 -0400

    Update task 3 to use correct method to retrieve word embeddings

--- Task 3/file_handler.py
+++ Task 3/file_handler.py
@@ -23,6 +23,11 @@ class Part3:
         results = [word_tokenize(t) for t in p]
         self.tokenized_posts = results
 
+    def tokenize_posts_partial_for_debugging(self, p, nr_posts):
+        posts = p[:nr_posts]
+        results = [word_tokenize(t) for t in posts]
+        self.tokenized_posts = results
+
     def get_tokenized_posts(self):
         return self.tokenized_posts
 
@@ -52,26 +57,26 @@ class Part3:
 
     def compute_embeddings_and_hit_rates_partial_for_debugging(self, nr_posts):
         model = self.w2model
-        for tp in self.tokenized_posts[:nr_posts]:
-            sum_embeddings = 0
+        for tokenized_post in self.tokenized_posts[:nr_posts]:
             nr_words = 0
             missed_words = 0
             hit_rate = 0
-            for w in tp:
-                try:
-                    tp = model.most_similar(w)[0]
-                    vl = tp[1]
-                    sum_embeddings += vl
-                    nr_words += 1
-                except KeyError:
-                    missed_words += 1
-                    continue
+
             try:
-                post_average = sum_embeddings / nr_words
-                hit_rate = ((nr_words - missed_words) / nr_words) * 100
-            except ZeroDivisionError:
-                post_average = 0
-            self.posts_average_emb_scores.append(post_average)
+                mean_embeddings = model.get_mean_vector(tokenized_post)
+                # tokenized_posts = model.most_similar(w)[0]
+                # vl = tokenized_posts[1]
+                # sum_embeddings += vl
+                # nr_words += 1
+            except KeyError:
+                missed_words += 1
+                continue
+            # try:
+            #     post_average = sum_embeddings / nr_words
+            #     hit_rate = ((nr_words - missed_words) / nr_words) * 100
+            # except ZeroDivisionError:
+            #     post_average = 0
+            self.posts_average_emb_scores.append(mean_embeddings)
             self.hit_rates.append(hit_rate)
 
     def get_hit_rates(self):
@@ -90,9 +95,11 @@ class Part3:
 
 def main():
     p3 = Part3()
-    p3.tokenize_posts(p3.get_posts())
+    debugging_nr_posts = 3
+    #p3.tokenize_posts(p3.get_posts())
+    p3.tokenize_posts_partial_for_debugging(p3.get_posts(), debugging_nr_posts)
     p3.display_nr_tokens()
-    p3.compute_embeddings_and_hit_rates_partial_for_debugging(1)
+    p3.compute_embeddings_and_hit_rates_partial_for_debugging(debugging_nr_posts)
     print(p3.get_embedding_scores())
     print(p3.get_hit_rates())
     # p3.display_embeddings_test()
